4d647a1cddaa96ef0dd4a0745fd29f5c8c802001
kd82 <kd82@zips.uakron.edu>
2021-05-26 16:39:55 -0700

    Final Segment Tree

--- data-structures/segment-tree.py
+++ data-structures/segment-tree.py
@@ -1,53 +1,52 @@
 from typing import List
 
-class SegmentTree():
+class SegmentTree:
     def __init__(self, arr):
         n = len(arr)
         self.tree = [0]*(4*n)
-        self.createTree(arr, 0, 0, n - 1)
-        
-    def merge(self, a, b):
-        return a + b
+        self.buildTree(0, arr, 0, n - 1)
 
-    def createTree(self, arr: List[int], index: int, lo: int, hi: int):
-        if lo == hi:  # base case
+    def buildTree(self,index, arr, lo, hi):
+        if lo == hi:
             self.tree[index] = arr[lo]
             return
         mid = (lo + hi)//2
-        self.createTree(arr, 2 * index + 1, lo, mid) # go to the left
-        self.createTree(arr, 2 * index + 2, mid + 1, hi) # go to the right
-        self.tree[index] = self.merge(self.tree[2 * index + 1], self.tree[2 * index + 2])
-        
-    def querySegTree(self, index: int, i: int, j: int, lo: int, hi: int):
-        if lo > j or hi < i: # completely outside range
+        self.buildTree(2*index + 1, arr, lo, mid)
+        self.buildTree(2*index + 2, arr, mid + 1, hi)
+        self.tree[index] = self.tree[2*index + 1] + self.tree[2*index + 2]
+
+    def update(self,treeIndex, index, val, lo, hi):
+        if lo == hi:
+            self.tree[treeIndex] = val
+            return
+        mid = (lo + hi)//2
+        if index > mid:
+            self.update(2*treeIndex + 2, index, val, mid + 1, hi)
+        elif index <= mid:
+            self.update(2*treeIndex + 1, index, val, lo, mid)
+        self.tree[treeIndex] = self.tree[2*treeIndex + 1] + self.tree[2*treeIndex + 2]
+
+    def query(self, treeIndex, i, j, lo, hi):
+        if j < lo or i > hi:
             return 0
         if i <= lo and j >= hi:
-            return self.tree[index]
+            return self.tree[treeIndex]
         mid = (lo + hi)//2
         if i > mid:
-            return self.querySegTree(2*index + 2, i, j, mid + 1, hi)
+            return self.query(2*treeIndex + 2, i, j, mid + 1, hi)
         elif j <= mid:
-            return self.querySegTree(2*index + 1, i, j, lo, mid)
-        leftQuery = self.querySegTree(2*index + 1, i, mid, lo, mid)
-        rightQuery = self.querySegTree(2*index + 2, mid + 1, j, mid + 1, hi)
-        return self.merge(leftQuery, rightQuery)
-    
-    def updateValue(self, index, i, val, lo, hi):
-        if lo == hi:
-            self.tree[index] = val
-        mid = (lo + hi)//2
-        if i > mid:
-            return self.querySegTree(2*index + 2, i, val, mid + 1, hi)
-        elif i <= mid:
-            return self.querySegTree(2*index + 1, i, val, lo, mid)
-        self.tree[index] = self.merge(self.tree[2 * index + 1], self.tree[2 * index + 2])
-        
+            return self.query(2*treeIndex + 1, i, j, lo, mid)
+        left = self.query(2*treeIndex + 2, mid + 1, j, mid + 1, hi)
+        right = self.query(2*treeIndex + 1, i , mid , lo, mid)
+        return left + right
+ 
 def main():
-    arr = [1,3,5]
+    arr = [1,2,5]
     tree = SegmentTree(arr)
     print(tree.tree)
-    print(tree.querySegTree(0, 0, 2, 0, len(arr) - 1))
-    tree.updateValue(0, 1, 2, 0, len(arr) - 1)
-    print(tree.querySegTree(0, 0, 2, 0, len(arr) - 1))
+    print(tree.query(0, 0, 2, 0, len(arr) - 1))
+    tree.update(0, 1, 2, 0, len(arr) - 1)
+    print(tree.tree)
+    print(tree.query(0, 1, 2, 0, len(arr) - 1))
 if __name__ == "__main__":
     main()
