39c7e666a485cf01b5ae6dfd4b3ef78bdb442aa2
kd82 <kd82@zips.uakron.edu>
2021-05-26 01:46:46 -0400

    Updated Seg Tree

--- data-structures/segment-tree.py
+++ data-structures/segment-tree.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class SegmentTree():
     def __init__(self, arr):
         n = len(arr)
@@ -18,6 +17,7 @@ class SegmentTree():
         self.createTree(arr, 2 * index + 1, lo, mid) # go to the left
         self.createTree(arr, 2 * index + 2, mid + 1, hi) # go to the right
         self.tree[index] = self.merge(self.tree[2 * index + 1], self.tree[2 * index + 2])
+        
     def querySegTree(self, index: int, i: int, j: int, lo: int, hi: int):
         if lo > j or hi < i: # completely outside range
             return 0
@@ -31,11 +31,23 @@ class SegmentTree():
         leftQuery = self.querySegTree(2*index + 1, i, mid, lo, mid)
         rightQuery = self.querySegTree(2*index + 2, mid + 1, j, mid + 1, hi)
         return self.merge(leftQuery, rightQuery)
-
+    
+    def updateValue(self, index, i, val, lo, hi):
+        if lo == hi:
+            self.tree[index] = val
+        mid = (lo + hi)//2
+        if i > mid:
+            return self.querySegTree(2*index + 2, i, val, mid + 1, hi)
+        elif i <= mid:
+            return self.querySegTree(2*index + 1, i, val, lo, mid)
+        self.tree[index] = self.merge(self.tree[2 * index + 1], self.tree[2 * index + 2])
+        
 def main():
-    arr = [18, 17, 13, 19, 15, 11, 20, 12, 33, 25]
+    arr = [1,3,5]
     tree = SegmentTree(arr)
-    #print(tree.tree)
+    print(tree.tree)
+    print(tree.querySegTree(0, 0, 2, 0, len(arr) - 1))
+    tree.updateValue(0, 1, 2, 0, len(arr) - 1)
     print(tree.querySegTree(0, 0, 2, 0, len(arr) - 1))
 if __name__ == "__main__":
     main()
