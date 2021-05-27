10c2f188268ee0f17c1236b80c9180df574b0493
kd82 <kd82@zips.uakron.edu>
2021-05-26 23:14:29 -0700

    Add eval RPN

---
+++ may-challenge/eval-rpn.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c not in "*/+-":
+                stack.append(int(c))
+            else:
+                l, r = stack.pop(), stack.pop()
+                if c == '*':
+                    stack.append(r*l)
+                elif c == '+':
+                    stack.append(r+l)
+                elif c == '-':
+                    stack.append(r-l)
+                else:
+                    stack.append(int(float(r)/l))
+        return sum(stack)
