386962d373fcafc3fd4aed596dd7cc8316e66a55
kd82 <kd82@zips.uakron.edu>
2021-05-31 22:53:22 -0700

    Add bfs for max area of an island

--- leetcode-random/max-area-of-island.py
+++ leetcode-random/max-area-of-island.py
@@ -17,8 +17,14 @@ def maxAreaOfIsland(grid: List[List[int]]) -> int:
                     grid[x][y] = -1
                     queue.append([x, y])
             return count
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != 1 or (i, j) in seen:
+                return 0
+            seen.add((i,j))
+            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i , j + 1)
         count = 0
         DIRS = [[-1,0],[1,0],[0, 1],[0, -1]]
+        seen = set()
         m, n = len(grid), len(grid[0])
         for i in range(m):
             for j in range(n):
