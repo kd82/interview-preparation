f2d300df0c5411425e3ff0c3799d1313eeb30970
kd82 <kd82@zips.uakron.edu>
2021-06-13 19:42:46 -0700

    Adds Cuts Problem

---
+++ june-challenge/max_area_cuts.py
@@ -0,0 +1,13 @@
+from collections import List
+
+class Solution:
+    def maxArea(self, h: int, w: int, horizontalCuts: List[int], verticalCuts: List[int]) -> int:
+        horizontalCuts.sort()
+        max_height = max(horizontalCuts[0], h - horizontalCuts[-1])
+        for i in range(1, len(horizontalCuts)):
+            max_height = max(max_height, horizontalCuts[i] - horizontalCuts[i - 1])
+        verticalCuts.sort()
+        max_width = max(verticalCuts[0], w - verticalCuts[-1])
+        for i in range(1, len(verticalCuts)):
+            max_width = max(max_width, verticalCuts[i] - verticalCuts[i - 1])
+        return (max_width*max_height)%(10**9 + 7)
